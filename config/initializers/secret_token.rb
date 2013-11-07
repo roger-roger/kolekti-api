f244bc102b53bd21359bf92543eb46dc33e543b9
Ryan Faerman <ry@nwitty.com>
2013-11-06 20:56:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KolektiApi::Application.config.secret_key_base = '17a415da9ee0bd0383e13a9e20bc288beacc658318300c5682d0b19e13f639a9d357b14301206af7d443843efe48ea85f73011280e3b0d1347ec9708562a5b90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KolektiApi::Application.config.session_store :cookie_store, key: '_kolekti-api_session'
