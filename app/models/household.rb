6de3187b46ded46a2a5637326c1026fcd1604f09
Ryan Faerman <ry@nwitty.com>
2013-11-16 21:52:11 -0500

    add default bundles on household create

--- app/models/household.rb
+++ app/models/household.rb
@@ -3,4 +3,12 @@ class Household < ActiveRecord::Base
   has_many :bundles
 
   validates :name, presence: true, uniqueness: true
+
+  after_create :setup_default_bundles
+
+  def setup_default_bundles
+    ['Grocery Store'].each do |bundle_name|
+      bundles.create(name: bundle_name)
+    end
+  end
 end
