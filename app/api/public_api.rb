2a3b9c6816d60bdf448b5afe19c44b90ecc893ec
Ryan Faerman <ry@nwitty.com>
2013-11-16 21:32:16 -0500

    change member joining to use URL params instead of post params

--- app/api/public_api.rb
+++ app/api/public_api.rb
@@ -132,12 +132,10 @@ class PublicAPI < Grape::API
           present current_household.users, with: Entities::UserEntity
         end
 
-        params do
-          requires :user_id, type: Integer
-        end
-        post do
+        post :user_id do
           if current_user
             current_household.users << current_user
+            current_household
           end
         end
       end
